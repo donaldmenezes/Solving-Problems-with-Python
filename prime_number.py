e0803b47bdb13b60c272016a7ee057962010df2a
donaldmenezes <43926873+donaldmenezes@users.noreply.github.com>
2018-11-01 16:28:03 +0530

    The square root method
    http://mathandmultimedia.com/2012/06/02/determining-primes-through-square-root/

--- prime_number.py
+++ prime_number.py
@@ -2,7 +2,7 @@ n = int(input('Enter a number: '))
 
 j = True
 
-for i in range (2, int(n**0.5)+1):      #Every composite number has a proper factor less than or equal to its square root.
+for i in range (2, int(n**0.5)+1):      #Every composite number has a proper factor less than or equal to its square root
   if n%i == 0:
     print (n, 'is not a prime number')
     j = False
