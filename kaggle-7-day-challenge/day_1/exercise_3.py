4278f289b233332c22dc08ca9adc5ebe61459b91
donaldmenezes <43926873+donaldmenezes@users.noreply.github.com>
2018-11-01 16:11:02 +0530

    Delete exercise_3.py

--- kaggle-7-day-challenge/day_1/exercise_3.py
+++
@@ -1,9 +0,0 @@
-alice_candies = 121
-bob_candies = 77
-carol_candies = 109
-
-#candies leftover if all candies are added together and divided equally
-
-to_smash = (alice_candies + bob_candies + carol_candies) % 3
-
-print(to_smash)
