dce275ac5b50023f55f7af083f97b80093bb9e22
donaldmenezes <43926873+donaldmenezes@users.noreply.github.com>
2018-11-01 16:03:14 +0530

    Using the modulus operator

---
+++ kaggle_seven_day_challenge/day_1/exercise_3.py
@@ -0,0 +1,11 @@
+alice_candies = 121
+bob_candies = 77
+carol_candies = 109
+
+#candies leftover if all candies are added together and divided equally
+
+to_smash = (alice_candies + bob_candies + carol_candies) % 3
+
+print(to_smash)
+
+#to_smash = 1 candy
