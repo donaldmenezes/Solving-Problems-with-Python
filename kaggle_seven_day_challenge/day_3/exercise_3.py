0c2d0001f51d2511afac1e763a9967b5c06a5c50
donaldmenezes <43926873+donaldmenezes@users.noreply.github.com>
2018-11-03 13:00:47 +0530

    Call int() on a bool

---
+++ kaggle_seven_day_challenge/day_3/exercise_3.py
@@ -0,0 +1,7 @@
+def exactly_one_topping(ketchup, mustard, onion):
+    """Return whether the customer wants exactly one of the three available toppings
+    on their hot dog.
+    """
+    return (int(ketchup) + int(mustard) + int(onion)) == 1
+    
+#kaggle solution : return (ketchup + mustard + onion) == 1
