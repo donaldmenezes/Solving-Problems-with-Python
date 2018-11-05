04da99786de6dfd863d2c58acf421a9284f77177
donaldmenezes <43926873+donaldmenezes@users.noreply.github.com>
2018-11-05 17:16:19 +0530

    Returns the sum of the squares
    Write a short Python function that takes a positive integer n and returns the sum of the squares of all the positive integers smaller than n.

---
+++ goodrich/Chapter_1/R-1.4.py
@@ -0,0 +1,17 @@
+def sum_of_squares(num):
+    '''
+    Assumes num is a positive integer
+    Returns the sum of squares of all positive integers smaller than num.
+    '''
+    
+    empty_list = []
+    for i in range(0,num):
+        empty_list.append(i**2)
+        
+    return sum(empty_list)
+    
+num = int(input('Enter a positive number: ')
+if num <= 0:
+    print('the number entered is negative')
+else:
+    print(sum_of_squares(num))
