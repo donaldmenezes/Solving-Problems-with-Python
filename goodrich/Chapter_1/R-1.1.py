778c49733fdad84f87c6605e916fc3227f24d6fd
donaldmenezes <43926873+donaldmenezes@users.noreply.github.com>
2018-11-05 13:44:03 +0530

    returns True if n is a multiple of m
    Write a short Python function, is_multiple(n, m), that takes two integer values and returns True if n is a multiple of m, that is, n = mi for some integer i, and False otherwise.

---
+++ goodrich/Chapter_1/R-1.1.py
@@ -0,0 +1,15 @@
+def is_multiple(n, m):
+    '''
+    Return True if n is a multiple of m,
+    else returns False
+    '''
+    try :
+        return int(m) % int(n) == 0
+    except Value Error:
+        return ("the two numbers should be integer values")
+        
+
+m = input("Enter a number: ")
+n = input("Enter a number: ")
+
+print(is_multiple(n,m))
