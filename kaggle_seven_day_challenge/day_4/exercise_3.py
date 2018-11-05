e86df7d88f50f60449cc3ba8c4a443e1086715f9
donaldmenezes <43926873+donaldmenezes@users.noreply.github.com>
2018-11-05 10:38:07 +0530

    Interchange last and first element of the list
    The next iteration of Mario Kart will feature an extra-infuriating new item, the *Purple Shell*. When used, it warps the last place racer into first place and the first place racer into last place. Complete the function below to implement the Purple Shell's effect.

---
+++ kaggle_seven_day_challenge/day_4/exercise_3.py
@@ -0,0 +1,13 @@
+def purple_shell(racers):
+    """Given a list of racers, set the first place racer (at the front of the list) to last
+    place and vice versa.
+    
+    >>> r = ["Mario", "Bowser", "Luigi"]
+    >>> purple_shell(r)
+    >>> r
+    ["Luigi", "Bowser", "Mario"]
+    """
+    temp = racers[0]
+    racers[0] = racers[-1]
+    racers[-1] = temp
+    return racers
