8f170c40705f09cccaae094a8c439b541464608f
donaldmenezes <43926873+donaldmenezes@users.noreply.github.com>
2018-11-09 21:00:51 +0530

    Update P-1.29.py

--- goodrich/Chapter_1/P-1.29.py
+++ goodrich/Chapter_1/P-1.29.py
@@ -3,3 +3,11 @@ import itertools
 list(itertools.permutations(['c', 'a', 't', 'd', 'o', 't']))
 
 list(itertools.permutations(['c', 'a', 't', 'd', 'o', 't'],2))
+
+
+## without the intertools module
+
+import random
+char_list = ['a','e','i','o','u']
+random.shuffle(char_list)
+print(''.join(char_list))
